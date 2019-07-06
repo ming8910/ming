b403d054d59e237ac03351ab1c98700b2ab521f1
ming8910 <52452506+ming8910@users.noreply.github.com>
2019-07-06 12:45:06 +0800

    Add files via upload

---
+++ ming-4/sketch_jul05a/sketch_jul05a.ino
@@ -0,0 +1,226 @@
+//1号
+void setup()
+
+{
+
+  pinMode(2, OUTPUT);
+
+  pinMode(3, OUTPUT);
+
+  pinMode(4, OUTPUT);
+
+  pinMode(5, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+
+
+byte income=0;
+
+
+
+void loop()
+
+{
+
+  if(Serial)
+
+  {
+
+  
+
+  }
+
+  else
+
+  {
+
+  
+
+  }
+
+  
+  digitalWrite(2, LOW);
+
+  digitalWrite(3, HIGH);
+
+  digitalWrite(4, HIGH);
+
+  digitalWrite(5, LOW);
+
+  delay(10);
+
+}
+
+
+
+//2号
+void setup()
+
+{
+
+  pinMode(2, OUTPUT);
+
+  pinMode(3, OUTPUT);
+
+  pinMode(4, OUTPUT);
+
+  pinMode(5, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+
+
+byte income=0;
+
+
+
+void loop()
+
+{
+
+  if(Serial)
+
+  {
+
+  
+
+  }
+
+  else
+
+  {
+
+  
+
+  }
+
+  
+  digitalWrite(2, LOW);
+
+  digitalWrite(3, HIGH);
+
+  digitalWrite(4, HIGH);
+
+  digitalWrite(5, LOW);
+
+  delay(10);
+
+}
+
+//3号
+void setup()
+
+{
+
+  pinMode(2, OUTPUT);
+
+  pinMode(3, OUTPUT);
+
+  pinMode(4, OUTPUT);
+
+  pinMode(5, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+
+
+byte income=0;
+
+
+
+void loop()
+
+{
+
+  if(Serial)
+
+  {
+
+  
+
+  }
+
+  else
+
+  {
+
+  
+
+  }
+
+  
+  digitalWrite(2, LOW);
+
+  digitalWrite(3, HIGH);
+
+  digitalWrite(4, HIGH);
+
+  digitalWrite(5, LOW);
+
+  delay(10);
+
+}
+
+
+//4号
+void setup()
+
+{
+
+  pinMode(2, OUTPUT);
+
+  pinMode(3, OUTPUT);
+
+  pinMode(4, OUTPUT);
+
+  pinMode(5, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+
+
+byte income=0;
+
+
+
+void loop()
+
+{
+
+  if(Serial)
+
+  {
+
+  
+
+  }
+
+  else
+
+  {
+
+  
+
+  }
+
+  
+  digitalWrite(2, LOW);
+
+  digitalWrite(3, HIGH);
+
+  digitalWrite(4, HIGH);
+
+  digitalWrite(5, LOW);
+
+  delay(10);
+
+}
